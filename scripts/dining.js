f73e6b3a9f3377650a7df0beb9abeb1c56b03a92
kendrah <kentinez11@gmail.com>
2023-10-30 11:19:02 -0400

    initial

---
+++ scripts/dining.js
@@ -0,0 +1,24 @@
+"use strict";
+
+window.onload(); {
+    const diningOptionsPayAsYouGo = document.getElementById("diningOptionsPayAsYouGo");
+    const diningOptionsAllInclusive = document.getElementById("diningOptionsAllInclusive");
+    //dinig....onchange = ...onChanged for both
+    const calculatePrice = document.getElementById("calculatePrice")
+
+    calculatePrice.onclick = calculatePriceClicked;
+
+};
+
+function finighOptionsChanged(){
+    //decide whi is selcted
+    //hide others
+    
+}
+
+function hidePayAsYouGo(){
+    const cfgvb = document.getElementById("");   
+    ffdcg.display = 'block';
+}
+
+//do for each to show and hide
